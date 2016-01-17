bc2b369e7adf1b84f7e6c833e0062df4020a7040
Your Name <you@example.com>
2016-01-17 13:01:47 +0000

    updated nav

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype>
+<html>
+<head>
+<title>eGo ONE </title>
+
+<style>
+
+.mynav{
+width:100%;
+height:60px;
+background-color:black;
+display:block;
+position:fixed;
+top:0px;
+
+}
+
+</style>
+</head>
+<body>
+<div class="mynav">eGo ONE</div>
+</body>
+</html>
